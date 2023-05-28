7614abf10096a3494e42dee137db0c36a20600fe
Nhật Trường <dinhnhattruong211@gmail.com>
2023-05-28 21:49:32 +0300

    update

--- admin/connect.php
+++ admin/connect.php
@@ -2,7 +2,7 @@
 $hostname = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'hms';
+$dbname = 'hmss';
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if(!$conn){
     die("Connection fail: ".mysqli_connect_error());
