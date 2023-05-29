d6477698aef95a87984ebaa9b3fc8cb3896f52af
Nhật Trường <dinhnhattruong211@gmail.com>
2023-05-30 00:51:05 +0300

    update

--- admin/employee/updateEmployee.php
+++ admin/employee/updateEmployee.php
@@ -11,11 +11,11 @@ $EmployeeCurrentAvatar = $_POST['EmployeeCurrentAvatar'];
 
 
 
-if (strlen($EmployeeName) > 0 && strlen($EmployeePosition) > 0 && strlen($EmployeeSalary) > 0 && strlen($EmployeePhone) > 0) {
+if (strlen($EmployeeName) > 0 && strlen($EmployeePosition) > 0 && strlen($EmployeeSalary) > 0 && strlen($EmployeePhone) > 0 && strlen($EmployeeId) > 0) {
     $query = "UPDATE employee SET EmployeeName='$EmployeeName', EmployeePosition='$EmployeePosition', EmployeeSalary='$EmployeeSalary', EmployeeAvatar='$EmployeeAvatar', EmployeeCurrentAvatar='$EmployeeCurrentAvatar' WHERE EmployeeId='$EmployeeId'";
     $data = mysqli_query($conn, $query);
     if ($data) {
-        if($EmployeeCurrentAvatar != "NO_CURRENT_IMAGE_EMPLOYEE_ITEM_UPDATE") {
+        if($EmployeeCurrentAvatar != "NO_CURRENT_IMAGE_EMPLOYEE_UPDATE") {
             unlink("images".$EmployeeCurrentAvatar);
         }
         echo "EMPLOYEE_UPDATE_SUCCESSFUL";
